d85c88d8b9f172de538c4060e7b4509ec74373ce
Tomas Mansur Campos <61087373+tomasmansurcampos@users.noreply.github.com>
2025-09-22 13:23:06 -0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -21,7 +21,7 @@ _nix()
  	nix-env -iA nixpkgs.fastfetch
   	nix-env -iA nixpkgs.putty
    	nix-env -iA nixpkgs.wireshark
-   	nix-env -iA nixpkgs.vscode
+   	nix-env -iA nixpkgs.geany
  	nix-env -iA nixpkgs.bleachbit
   	nix-env -iA nixpkgs.keepass
    	nix-env -iA nixpkgs.keepassxc
