361cdef911df7f1ec92ed698295ddd0ef7e4209b
Tomas Mansur Campos <61087373+tomasmansurcampos@users.noreply.github.com>
2025-11-02 03:36:42 -0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ESSENTIAL_PACKAGES="build-essential dnsutils man nano gnupg gcc nasm gdb python-is-python3 stubby curl wget screen minicom git make binutils tcpdump lynx lsb-release htop bmon locales-all ascii ipcalc sipcalc rar unrar zip unzip p7zip p7zip-full p7zip-rar ffmpeg sox libsox-fmt-all"
+ESSENTIAL_PACKAGES="build-essential dnsutils stubby man nano gnupg gcc nasm gdb python-is-python3 stubby curl wget screen minicom git make binutils tcpdump lynx lsb-release htop bmon locales-all ascii ipcalc sipcalc rar unrar zip unzip p7zip p7zip-full p7zip-rar ffmpeg sox libsox-fmt-all"
 
 PACKAGES="gnome-tweaks vlc speak audacity keepassxc libreoffice qbittorrent geany"
 
