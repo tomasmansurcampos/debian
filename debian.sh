c3bbe2205f8231c37565a8278aa7d882d008e2ff
Tomas Mansur Campos <61087373+tomasmansurcampos@users.noreply.github.com>
2025-11-02 03:44:50 -0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -2,7 +2,7 @@
 
 ESSENTIAL_PACKAGES="build-essential dnsutils stubby man nano gnupg gcc nasm gdb python-is-python3 stubby curl wget screen minicom git make binutils tcpdump lynx lsb-release htop bmon locales-all ascii ipcalc sipcalc rar unrar zip unzip p7zip p7zip-full p7zip-rar ffmpeg libavcodec-extra sox libsox-fmt-all"
 
-PACKAGES="gnome-tweaks vlc spek audacity keepassxc libreoffice libreoffice-l10n-en qbittorrent geany"
+PACKAGES="gnome-tweaks vlc spek audacity keepassxc libreoffice qbittorrent geany"
 
 GNOME="apt install --no-install-recommends -y gnome-core && apt install -y $PACKAGES && apt install -y $ESSENTIAL_PACKAGES"
 
