3d612991a95ddb08ad5c7c29d4bc255649d21e64
Tomas Mansur Campos <61087373+tomasmansurcampos@users.noreply.github.com>
2025-11-27 11:16:58 -0300

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -462,6 +462,19 @@ EOF
 	chmod +x /usr/bin/installer-google-chrome
 	bash /usr/bin/installer-google-chrome
 
+	### ONLYOFFICE
+	cat <<"EOF" > /usr/bin/installer-onlyoffice
+mkdir -p -m 700 ~/.gnupg
+gpg --no-default-keyring --keyring gnupg-ring:/tmp/onlyoffice.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+chmod 644 /tmp/onlyoffice.gpg
+chown root:root /tmp/onlyoffice.gpg
+mv /tmp/onlyoffice.gpg /usr/share/keyrings/onlyoffice.gpg
+echo 'deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] https://download.onlyoffice.com/repo/debian squeeze main' | tee -a /etc/apt/sources.list.d/onlyoffice.list
+apt update && apt install -y onlyoffice-desktopeditors
+EOF
+	chmod +x /usr/bin/installer-onlyoffice
+	bash /usr/bin/installer-onlyoffice
+
 	### TOR BROWSER
 	cat <<"EOF" > /usr/bin/installer-tor-browser
 #!/bin/bash
